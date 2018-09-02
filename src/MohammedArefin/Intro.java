54208010b0e86ed23053d9bf1d27e5eda36e73c4
masrefin <mohammed.arefin@gmail.com>
2018-09-01 23:35:13 -0400

    created new repo

---
+++ src/MohammedArefin/Intro.java
@@ -0,0 +1,14 @@
+package MohammedArefin;
+
+public class Intro {
+
+    Intro(String name){
+        System.out.println("Hello may name is"+name);
+    }
+
+    public static void main(String[] args) {
+        Intro n = new Intro("Mohammed Arefin");
+    }
+
+
+}
